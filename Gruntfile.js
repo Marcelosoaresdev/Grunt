70347c33a39c6cff12ca55b95858934e14b01102
Marcelo Soares <marcelohsoares142@gmail.com>
2024-07-02 19:43:10 -0300

    Criando tarefas

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,17 @@
-module.exports = function(grunt) {
+const { set } = require("grunt");
+
+module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-    })
-}
+    pkg: grunt.file.readJSON("package.json"),
+    });
+
+    grunt.registerTask("olaGrunt", function () {
+    const done = this.async();
+    setTimeout(function () {
+        console.log("ola grunt");
+        done();
+    }, 3000);
+    });
+
+    grunt.registerTask("default", ["olaGrunt"]);
+};
