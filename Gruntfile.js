ff03e55d66079d94cefc7f69b4c648ed9ee3f68b
Marcelo Soares <marcelohsoares142@gmail.com>
2024-07-02 20:35:12 -0300

    tarefas de forma paralela

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,9 @@ module.exports = function (grunt) {
                 'main2.css' : 'main.scss'
             }
         }
+    },
+    concurrent: {
+        target: ["olaGrunt", "less", "sass", "tarefaDemorada"]
     }
     });
 
@@ -37,9 +40,18 @@ module.exports = function (grunt) {
         done();
     }, 3000);
     });
+    
+    grunt.registerTask("tarefaDemorada", function () {
+        const done = this.async();
+        setTimeout(function () {
+            console.log("Tarefa demorada");
+            done();
+        }, 3000);
+        });
 
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-sass");
+    grunt.loadNpmTasks("grunt-concurrent");
 
-    grunt.registerTask("default", ["less", "sass"]);
+    grunt.registerTask("default", ["concurrent"]);
 };
